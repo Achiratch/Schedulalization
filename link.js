8308e7209318a729934050dda02e3aad693c536e
Achiratch <48543080+Achiratch@users.noreply.github.com>
2020-03-28 16:57:01 +0700

    Add files via upload

--- link.js
+++ link.js
@@ -1,3 +1,14 @@
 function relocate_profile() {
   location.href = "profile.html";
 }
+
+function relocate_login() {
+  location.href = "login.html";
+}
+function relocate_register() {
+  location.href = "register.html";
+}
+
+function relocate_AddSchedule() {
+  location.href = "AddCourse.html";
+}
